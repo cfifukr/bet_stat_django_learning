8314c71e1d239d1c5ba8fc348ac47df904df27d6
Vladyslav PC Kyiv <prosto.acc.vlada@gmail.com>
2023-09-18 01:59:54 +0300

    SignUp

--- bets/views.py
+++ bets/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
 
 def main_page(request):
-    return render(request)
+
+    return render(request, 'bets/main.html')
+
+
+@csrf_exempt
+def sign_up_page(request):
+    if request.method == "GET":
+        return render(request, 'bets/sign_up.html')
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            data = User.objects.create_user(username=request.POST["username"], email=request.POST["email"],password=request.POST["password1"])
+            data.save()
+            return redirect("main_page")
+
+        else :
+            return render(request, 'bets/sign_up.html', {"error": "Password didn`t match"})
--- betting/urls.py
+++ betting/urls.py
@@ -1,12 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path
+from bets import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main_page, name = "main_page"),
+
 
 
     #auth
+    path('signup/', views.sign_up_page, name = "sign_up_page")
 
 
 ]
